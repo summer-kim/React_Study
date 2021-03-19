29346a7924def3fef253919da846f666f7842f39
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-03-19 20:51:40 +0900

    add file-loader on webPack

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,18 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'asset/',
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [new RefreshWebpackPlugin()],
