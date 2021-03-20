1d0e200f7102ad74f6fd65f6a1d465a95ad1b4ef
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-03-20 22:33:15 +0900

    change Class Component to Function (LottoGames)

--- components/Lotto.jsx
+++ components/Lotto.jsx
@@ -1,7 +1,13 @@
-import React, { Component } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useMemo,
+} from 'react';
 import Ball from './subParts/Ball';
 
-function getLottoNumbers() {
+const getLottoNumbers = () => {
   const candidate = Array(45)
     .fill()
     .map((v, i) => i + 1);
@@ -14,109 +20,96 @@ function getLottoNumbers() {
     shuffle.push(num);
   }
   return shuffle.sort((a, b) => a - b);
-}
+};
 
-class Lotto extends Component {
-  state = {
-    numberSelected: getLottoNumbers(),
-    ballSelected: [],
-    startable: true,
-    resetable: false,
-    run: false,
-    reset: false,
-  };
-  timeoutArray = [];
+const Lotto = () => {
+  const [numbers, setNumbers] = useState(getLottoNumbers());
+  const [balls, setBalls] = useState([]);
+  const [startable, setStartable] = useState(true);
+  const [resetable, setResetable] = useState(false);
+  const [run, setRun] = useState(false);
+  const [reset, setReset] = useState(false);
+  const timeoutArray = useRef([1, 2]);
 
-  onStart = () => {
-    const { numberSelected } = this.state;
-    for (let i = 0; i < numberSelected.length; i++) {
-      this.timeoutArray[i] = setTimeout(() => {
-        this.setState((prevState) => ({
-          ballSelected: [...prevState.ballSelected, numberSelected[i]],
-        }));
-        if (i === numberSelected.length - 1) {
-          this.setState({ resetable: true });
-        }
-      }, (i + 1) * 1000);
-    }
-  };
-
-  componentDidUpdate() {
-    const { run, reset } = this.state;
+  useEffect(() => {
     if (run) {
-      this.onStart();
-      this.setState({ run: false });
-    } else if (reset) {
-      this.onReset();
-      this.setState({ reset: false });
+      for (let i = 0; i < numbers.length; i++) {
+        console.log(timeoutArray.current[i]);
+        timeoutArray.current[i] = setTimeout(() => {
+          setBalls((preBalls) => [...preBalls, numbers[i]]);
+          if (i === numbers.length - 1) setResetable(true);
+        }, (i + 1) * 1000);
+      }
     }
-  }
+    return () => {
+      if (run) {
+        clearTimeFunction();
+        setRun(false);
+      }
+    };
+  }, [run]);
 
-  onClick = () => (e) => {
+  useEffect(() => {
+    if (reset) {
+      onReset();
+    }
+  }, [reset]);
+
+  const onClick = () => (e) => {
     if (e.target.id === 'start') {
-      this.setState({ run: true, startable: false });
+      setRun(true);
+      setStartable(false);
     } else {
-      this.setState({ reset: true, resetable: false });
+      setReset(true);
+      setResetable(false);
     }
   };
 
-  componentWillUnmount() {
-    this.clearTimeFunction();
-  }
-
-  onReset = () => {
-    this.clearTimeFunction();
-    this.setState({
-      numberSelected: getLottoNumbers(),
-      ballSelected: [],
-      startable: true,
-      resetable: false,
-      run: false,
-    });
-    this.setState({ startable: true });
+  const onReset = () => {
+    clearTimeFunction();
+    setNumbers(getLottoNumbers());
+    setBalls([]);
+    setStartable(true);
+    setResetable(false);
+    setRun(false);
+    setReset(false);
   };
 
-  clearTimeFunction = () => {
-    const { timeoutArray } = this;
-    timeoutArray.length > 0 &&
-      timeoutArray.forEach((timeout) => {
+  const clearTimeFunction = () => {
+    timeoutArray.current.length > 0 &&
+      timeoutArray.current.forEach((timeout) => {
         clearTimeout(timeout);
       });
   };
 
-  render() {
-    const { ballSelected, startable, resetable } = this.state;
-    return (
-      <div className='container text-center h-100'>
-        <h3 className='m-2 text-center text-uppercase text-secondary'>
-          Result
-        </h3>
-        <div className='d-flex m-1 justify-content-center align-items-center h-50'>
-          {ballSelected.map((num, i) => (
-            <Ball num={num} key={i + ':' + num} />
-          ))}
-        </div>
-        <div>
-          <button
-            className='btn btn-outline-secondary m-1'
-            id='start'
-            disabled={!startable}
-            onClick={this.onClick()}
-          >
-            Start
-          </button>
-          <button
-            className='btn btn-outline-secondary m-1'
-            id='reset'
-            disabled={!resetable}
-            onClick={this.onClick()}
-          >
-            Reset
-          </button>
-        </div>
+  return (
+    <div className='container text-center h-100'>
+      <h3 className='m-2 text-center text-uppercase text-secondary'>Result</h3>
+      <div className='d-flex m-1 justify-content-center align-items-center h-50'>
+        {balls.map((num, i) => (
+          <Ball num={num} key={i + ':' + num} />
+        ))}
       </div>
-    );
-  }
-}
+      <div>
+        <button
+          className='btn btn-outline-secondary m-1'
+          id='start'
+          disabled={!startable}
+          onClick={onClick()}
+        >
+          Start
+        </button>
+        <button
+          className='btn btn-outline-secondary m-1'
+          id='reset'
+          disabled={!resetable}
+          onClick={onClick()}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Lotto;
