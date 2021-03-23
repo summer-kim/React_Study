1f338ef644caee7cd3600ab9171b5c7eabb9d852
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-03-24 02:38:27 +0900

    change the way of Setting Row&Column

--- components/findingMine/Form.jsx
+++ components/findingMine/Form.jsx
@@ -3,49 +3,87 @@ import { TableContext } from './MineSearch';
 import { ACTION_TYPE } from './code';
 
 const Form = () => {
-  const [rows, setRows] = useState(10);
-  const [cols, setCols] = useState(10);
-  const [mines, setMines] = useState(20);
-  const { dispatch } = useContext(TableContext);
-
-  const onChangeRows = useCallback((e) => {
-    setRows(e.target.value);
-  }, []);
-
-  const onChangeCols = useCallback((e) => {
-    setCols(e.target.value);
-  }, []);
+  const [mines, setMines] = useState(10);
+  const { dispatch, result } = useContext(TableContext);
 
   const onChangeMines = useCallback((e) => {
     setMines(e.target.value);
   }, []);
 
-  const onClick = useCallback(() => {
-    dispatch({ type: ACTION_TYPE.START_GAME, rows, cols, mines });
-  }, [rows, cols, mines]);
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const length = e.target['gameType'].value;
+      dispatch({
+        type: ACTION_TYPE.START_GAME,
+        rows: length,
+        cols: length,
+        mines,
+      });
+    },
+    [mines]
+  );
 
   return (
-    <div>
+    <form className='m-1 form-check' onSubmit={onSubmit}>
       <input
-        type='number'
-        value={rows}
-        placeholder='rows'
-        onChange={onChangeRows}
+        className='form-check-input'
+        type='radio'
+        value={5}
+        id='five'
+        name='gameType'
       />
+      <label className='form-check-label' htmlFor='five'>
+        5 X 5
+      </label>
+      <br />
       <input
-        type='number'
-        value={cols}
-        placeholder='columns'
-        onChange={onChangeCols}
+        className='form-check-input'
+        type='radio'
+        value={10}
+        id='ten'
+        name='gameType'
       />
+      <label className='form-check-label' htmlFor='ten'>
+        10 X 10
+      </label>
+      <br />
+      <input
+        className='form-check-input'
+        type='radio'
+        value={15}
+        id='fifteen'
+        name='gameType'
+      />
+      <label className='form-check-label' htmlFor='fifteen'>
+        15 X 15
+      </label>
+      <br />
+      <input
+        className='form-check-input'
+        type='radio'
+        value={20}
+        id='twenty'
+        name='gameType'
+      />
+      <label className='form-check-label' htmlFor='twenty'>
+        20 X 20
+      </label>
+      <br />
+      <label className='form-check-label' htmlFor='mines'>
+        Number of Mines
+      </label>
+      <br />
       <input
         type='number'
         value={mines}
-        placeholder='number of mines'
         onChange={onChangeMines}
+        name='mines'
       />
-      <button onClick={onClick}>Submit</button>
-    </div>
+      <br />
+      <button className='btn border rounded'>Submit</button>
+      <div className=' border rounded'>{result}</div>
+    </form>
   );
 };
 
